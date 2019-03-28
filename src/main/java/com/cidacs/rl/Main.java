899c9a585cf65c99655362ca6f5665ea39d4eff4
gcgbarbosa <gcgbarbosa@gmail.com>
2019-03-28 17:08:23 -0300

    update spark and added spark dataframe code

--- src/main/java/com/cidacs/rl/Main.java
+++ src/main/java/com/cidacs/rl/Main.java
@@ -7,14 +7,19 @@ import com.cidacs.rl.io.CsvReader;
 import com.cidacs.rl.linkage.Linkage;
 import com.cidacs.rl.record.ColumnRecordModel;
 import com.cidacs.rl.record.RecordModel;
-import com.cidacs.rl.record.RecordPairModel;
 import com.cidacs.rl.search.Indexing;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.hadoop.hdfs.DFSClient.Conf;
+
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import scala.reflect.ClassManifestFactory;
 
 import java.util.ArrayList;
 
@@ -22,6 +27,35 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        SparkSession spark = SparkSession
+        .builder()
+        .appName("Cidacs-RL")
+        .config("spark.master", "local[*]")
+        .getOrCreate();
+
+        // read dataset
+        // assets/dsa.csv
+        Dataset<Row> dsb = spark.read().format("csv")
+            .option("sep", ",")
+            .option("inferSchema", "false")
+            .option("header", "true")
+            .load("assets/dsb.csv");
+
+
+        System.out.println(
+            dsb.javaRDD().map(new Function<Row, String>() {
+                public String call(Row row){
+                    
+                    return row.getAs("name");
+                }
+            }).take(10) );
+
+        System.out.println("DS:");
+        dsb.show();
+
+        spark.stop();
+
+        /*
         // Spark
         SparkConf conf = new SparkConf().setAppName("cidacs-rl").setMaster("local[*]");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -95,5 +129,6 @@ public class Main {
         }).saveAsTextFile("assets/result");
 
         sc.close();
+        */
     }
 }
