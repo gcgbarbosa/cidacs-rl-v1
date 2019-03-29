16edae636da7c3047bbfb85109a7c827eca3875f
gcgbarbosa <gcgbarbosa@gmail.com>
2019-03-29 11:19:20 -0300

    cleaning and comments

--- src/main/java/com/cidacs/rl/Main.java
+++ src/main/java/com/cidacs/rl/Main.java
@@ -10,16 +10,11 @@ import com.cidacs.rl.record.RecordModel;
 import com.cidacs.rl.search.Indexing;
 import org.apache.commons.csv.CSVRecord;
 
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
-import scala.reflect.ClassManifestFactory;
 
 import java.util.ArrayList;
 
@@ -39,6 +34,7 @@ public class Main {
         dbACsvRecords = csvReader.getCsvIterable(config.getDbA());
         indexing.index(dbACsvRecords);
 
+        // Start Spark session
         SparkSession spark = SparkSession
             .builder()
             .appName("Cidacs-RL")
@@ -51,40 +47,50 @@ public class Main {
             .option("sep", ",")
             .option("inferSchema", "false")
             .option("header", "true")
-            .load("assets/dsb.csv");
+            .load(config.getDbB());
 
         dsb.javaRDD().map(new Function<Row, String>() {
             public String call(Row row){
-                // Reading config
+                // Reading config again
+                // Using the config from outer scope throws java not serializable error
                 ConfigReader confReader = new ConfigReader();
                 ConfigModel config = confReader.readConfig();
-
+                // same with linkage
                 Linkage linkage = new Linkage(config);
 
+                // place holder variables to instanciate an record object
                 RecordModel tmpRecord = new RecordModel();
                 ArrayList<ColumnRecordModel> tmpRecordColumns = new ArrayList<ColumnRecordModel>();
                 
+                // convert row to RecordModel
                 for(ColumnConfigModel column : config.getColumns()){
                     try {
                         String tmpValue = row.getAs(column.getIndexB());
+                        // Remove anything that is not a uppercase letter and a digit
                         tmpValue = tmpValue.replaceAll("[^A-Z0-9 ]", "").replaceAll("\\s+", " ");
+                        // if the value is equal to one space, add empty string instead
                         if(tmpValue.equals(" ")){
                             tmpValue = "";
                         }
+                        //
                         String tmpId = column.getId();
+                        // maybe it is not necessary to have the tipe of the variable replicated
+                        // FIXME: add function to config that allows for consulting the type of the variable
+                        // using the ID
                         String tmpType = column.getType();
+                        // add new column 
                         tmpRecordColumns.add(new ColumnRecordModel(tmpId, tmpType, tmpValue));
                     } catch (ArrayIndexOutOfBoundsException e){
                         e.printStackTrace();
                     }
                 }
+                // set the column to record
                 tmpRecord.setColumnRecordModels(tmpRecordColumns);
-
-
+                //
                 return linkage.linkSpark(tmpRecord);
             }
         }).saveAsTextFile("assets/result");
-
+        //
         spark.stop();
     }
 }
